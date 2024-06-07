2f59d907d700ad4f8acb8d250d66a5eff075608f
ashbie1 <ashbiesinsoa@gmail.com>
2024-06-07 13:04:32 +0800

    1st commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,12 @@ import './App.css';
 import DataForm from './components/DataForm';
 import 'antd/dist/antd.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Author Management</h1>
-      </header>
+    <> 
       <DataForm />
-    </div>
-  );
+    </>
+  )
 }
 
 export default App;
