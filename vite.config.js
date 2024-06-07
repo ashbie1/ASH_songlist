c75ab6bb059462f46f267ecc620a8b9b7075bba2
ashbie1 <ashbiesinsoa@gmail.com>
2024-06-07 16:35:24 +0800

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
-export default {
-  build: {
-    rollupOptions: {
-      external: ['axios']
-    }
-  }
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
